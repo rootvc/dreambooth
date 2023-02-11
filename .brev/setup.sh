83f63d1280704bfc07fd145f75ac4605349f2a79
yasyf <yasyfm@gmail.com>
2023-02-11 07:39:22 +0000

    Change setup script.

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -2,13 +2,5 @@
 
 set -eo pipefail
 
-export PATH=~/.local/bin:$PATH
-export ACCELERATE_LOG_LEVEL=debug
-
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
-sudo dpkg -i cuda-keyring_1.0-1_all.deb
-sudo apt-get update
-sudo apt-get -y install cuda-11.8
-
-cd dreambooth
-pip install -r requirements.txt
+echo 'export PATH=~/.local/bin:$PATH' >>~/.bashrc
+echo 'export ACCELERATE_LOG_LEVEL=debug' >>~/.bashrc
