791aa7cc5103e05126a8a1b238aab01db8824395
Yasyf Mohamedali <yasyfm@gmail.com>
2023-02-12 16:31:55 -0800

    Test on a cheaper instance.

--- dreambooth/train/trainer.py
+++ dreambooth/train/trainer.py
@@ -9,17 +9,26 @@ class TrainJob:
         self.trainml = TrainML()
         self.id = id
 
-    async def run(self):
+    async def run(self, instance: str = "ml.p4d.24xlarge", dtype: str = "bf16"):
         estimator = Estimator(
             image_uri="630351220487.dkr.ecr.us-west-2.amazonaws.com/train-dreambooth-sagemaker:latest",
             role="SageMakerRole",
             use_spot_instances=True,
             max_run=60 * 60 * 1,
             train_instance_count=1,
-            train_instance_type="ml.p4d.24xlarge",
+            train_instance_type=instance,
+            environment={
+                "INSTANCE_TYPE": instance,
+                "ACCELERATE_MIXED_PRECISION": dtype,
+            },
         )
         estimator.fit({"train": f"s3://dreambooth-datasets/{self.id}"})
 
 
 if __name__ == "__main__":
-    asyncio.run(TrainJob("test").run())
+    asyncio.run(
+        TrainJob("test").run(
+            instance="ml.p3.2xlarge",
+            dtype="fp16",
+        )
+    )
--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 export PYTHONPATH=$PYTHONPATH:.
-cp /root/.cache/huggingface/accelerate/default_config.yaml ~/.cache/huggingface/accelerate/default_config.yaml
+
+CONFIG="dreambooth/config/accelerate/${INSTANCE_TYPE}.yml"
+if [ -f "${CONFIG}"]; then
+  cp -f "${CONFIG}" ~/.cache/huggingface/accelerate/default_config.yaml
+fi
 
 accelerate launch dreambooth/train/train.py
