f4d7913763271a6385fd6c22a40a2ba3921beaf8
Yasyf Mohamedali <yasyfm@gmail.com>
2023-02-16 03:00:39 -0800

    Unpack model properly

--- dreambooth/train/train.py
+++ dreambooth/train/train.py
@@ -12,10 +12,11 @@ def main():
     train_data = Path(env.channel_input_dirs["train"])
     model_data = Path(env.channel_input_dirs["model"])
 
-    model_dir = tempfile.mkdtemp()
-    shutil.unpack_archive(model_data, model_dir)
-
     params = get_params()
+    model_file = model_data / params.model.name.with_suffix(".tar.xz").name
+
+    model_dir = tempfile.mkdtemp()
+    shutil.unpack_archive(model_file, model_dir)
     params.model.name = model_dir
 
     model = get_model(instance_path=train_data, params=params)
