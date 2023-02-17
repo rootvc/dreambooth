a09b480e3b4e6e60ab0b86213cb37e299c30397e
Yasyf Mohamedali <yasyfm@gmail.com>
2023-02-17 12:24:08 -0800

    restore config.

--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -5,9 +5,14 @@ export PYTHONPATH=$PYTHONPATH:.
 echo "Training model from $PWD"
 ls -la
 
-CONFIG="dreambooth/config/accelerate/${INSTANCE_TYPE}.yml"
+CONFIG_ROOT="dreambooth/config/accelerate/${INSTANCE_TYPE}.yml"
+DEFAULT_CONFIG="${CONFIG_ROOT}/default.yaml"
+CONFIG="${CONFIG_ROOT}/${INSTANCE_TYPE}.yml"
+
 if [ -f "${CONFIG}" ]; then
   cp -f "${CONFIG}" ~/.cache/huggingface/accelerate/default_config.yaml
+else
+  cp -f "${DEFAULT_CONFIG}" ~/.cache/huggingface/accelerate/default_config.yaml
 fi
 
 accelerate launch dreambooth/train/train.py
