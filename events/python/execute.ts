41e7b1182e2aedcb5de5096b82c90d90a7c994d7
Yasyf Mohamedali <yasyfm@gmail.com>
2023-02-19 19:39:44 -0800

    Download pyodide.

--- events/python/execute.ts
+++ events/python/execute.ts
@@ -1,3 +1,7 @@
+import got from "got";
+import { spawn } from "node:child_process";
+import fs from "node:fs";
+import { pipeline } from "node:stream/promises";
 import { defineFunction } from "../tools";
 
 const PYODIDE_VERSION = "0.22.1";
@@ -7,6 +11,10 @@ export default defineFunction(
   "Execute Python Module",
   "dreambooth/python.execute",
   async ({ event }) => {
-    //
+    await pipeline(
+      got.stream.get(PYODIDE_URL),
+      fs.createWriteStream("pyodide.tar.bz2")
+    );
+    await spawn("tar", ["-xjvf", "pyodide.tar.bz2", "-C", "pyodide"]);
   }
 );
