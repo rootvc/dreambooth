cc5819ef71c248c71649c5cdf03afadd834474f5
Yasyf Mohamedali <yasyfm@gmail.com>
2023-02-20 17:34:05 -0800

    Add DS_BUILD_UTILS.

---
+++ events/src/pages/api/media.ts
@@ -0,0 +1,25 @@
+import { Fields, File, Files, IncomingForm } from "formidable";
+import { readFile } from "fs";
+import { Inngest } from "inngest";
+import { NextApiRequest, NextApiResponse } from "next";
+import pify from "pify";
+
+const inngest = new Inngest({ name: "rootvc-fn-media" });
+
+const media = async (request: NextApiRequest, response: NextApiResponse) => {
+  const form = new IncomingForm();
+  const parse = pify(form.parse.bind(form), { multiArgs: true }) as (
+    req: NextApiRequest
+  ) => Promise<[Fields, Files]>;
+  const [fields, files] = await parse(request);
+  const media = files.media as File;
+
+  inngest.send("dreambooth/booth.photos", {
+    data: {
+      email: fields.email as string,
+      blob: (await pify(readFile)(media.filepath)).toString("base64"),
+    },
+  });
+};
+
+export default media;
