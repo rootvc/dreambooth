aa38e82e1f43e78ef55c5f4bfd1df9828ce90068
Yasyf Mohamedali <yasyfm@gmail.com>
2023-02-21 18:36:34 -0800

    Make inside container again.

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
+set -x
 
 export DOCKER_BUILDKIT=1
 
-docker build -f dockerfiles/Dockerfile.train . -t train-dreambooth
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 630351220487.dkr.ecr.us-west-2.amazonaws.com
+
+docker buildx create --use --name builder \
+  --buildkitd-flags '--oci-worker-snapshotter=stargz' \
+  --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=-1 \
+  --driver-opt env.BUILDKIT_STEP_LOG_MAX_SPEED=-1
+
+docker buildx inspect --bootstrap builder
+
+docker build \
+  -o type=registry,oci-mediatypes=true,compression=estargz,force-compression=true,push=true \
+  --builder builder \
+  -f dockerfiles/Dockerfile.pytorch . \
+  -t rootventures/pytorch
+
+docker build \
+  -o type=registry,oci-mediatypes=true,compression=estargz,force-compression=true,push=true \
+  --builder builder \
+  -f dockerfiles/Dockerfile.train . \
+  -t rootventures/train-dreambooth
+
+docker build \
+  -o type=registry,oci-mediatypes=true,compression=estargz,force-compression=true,push=true \
+  --builder builder \
+  -f dockerfiles/Dockerfile.sagemaker . \
+  -t rootventures/train-dreambooth-sagemaker
+
+docker tag rootventures/train-dreambooth-sagemaker 630351220487.dkr.ecr.us-west-2.amazonaws.com/train-dreambooth-sagemaker:latest
+docker push 630351220487.dkr.ecr.us-west-2.amazonaws.com/train-dreambooth-sagemaker:latest
+
+docker build \
+  -o type=registry,oci-mediatypes=true,compression=estargz,force-compression=true,push=true \
+  --builder builder \
+  -f dockerfiles/Dockerfile . \
+  -t rootventures/dreambooth
